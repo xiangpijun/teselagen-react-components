e0f19e198aa851eaeb6c41c674096f75f964e8e1
tnrich <tnrich@teselagen.com>
2019-01-15 17:28:46 -0800

    removing react from a version of withQuery; adding start of type defs

---
+++ typings.d.ts
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+declare class DataTable extends React.Component<DataTableProps, any> {}
+
+export interface DataTableProps {
+  /**
+     * disable the table
+     */
+  disabled?: boolean; 
+}
+
+export DataTable;
+
+
+type FunctionType1 = (x: string, y: number) => number;
+
+export FunctionType1
+
+//  /**
+// * @param  {} [recordIds]
+// */
+// declare function selectTableRecords (hi){
+
+// }
+
+// export selectTableRecords
